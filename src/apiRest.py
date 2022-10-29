5dcd1bbc08f3927b99db85290bb47f7cd4576429
JuanbaCGC <JuanBautista.Castano@alu.uclm.es>
2022-10-29 05:09:01 -0400

    Error in the signup and user token

--- src/apiRest.py
+++ src/apiRest.py
@@ -3,8 +3,9 @@
 from flask import Flask
 from flask import jsonify, request
 from flask_restful import Resource, Api
+import secrets
 
-from users import users
+from users import usersList
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,7 +13,7 @@ api = Api(app)
 
 @app.route('/version', methods=['GET'])
 def getVersion():
-    return jsonify({"version":"1.0"})
+    return jsonify({"Version":"1.0"})
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -20,8 +21,12 @@ def signup():
         "name": request.json['name'],
         "password": request.json['password']
     }
-    users.append(newUser)
-    return jsonify({"message": "User Added Succesfully", "users": users})
+    userFound = [users for users in usersList if users['name'] == request.json['name']]
+    if (len(userFound) > 0):
+        return jsonify({"ErrorMessage": "There is a user with the same name. Try other user name."})
+    usersList.append(newUser)
+    token = secrets.token_urlsafe(20)
+    return jsonify({"Message": "User Added Succesfully", "User token": token})
 
 
 if __name__ == '__main__':
--- src/users.py
+++ src/users.py
@@ -1,4 +1,4 @@
-users = [
+usersList = [
     {"name": "JuanbaCGC", "password": "12345"},
     {"name": "AlbertoMN", "password": "hola"},
     {"name": "ArturoGJ", "password": "hola123"}
