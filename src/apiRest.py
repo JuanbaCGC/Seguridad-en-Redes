e3120161a33818963585f32647a820124f8e4c61
JuanbaCGC <JuanBautista.Castano@alu.uclm.es>
2022-11-13 10:41:26 -0500

    validate tokens implementation

--- src/apiRest.py
+++ src/apiRest.py
@@ -50,9 +50,29 @@ def writeToken(token, username):
     timer = threading.Timer(300.0, revokeToken,(newToken, ))
     timer.start()
 
-def user_exist(username):
-    userFound = [users for users in UserList if users['username'] == username]
-    return username
+def verifyToken(token):
+    data = read('tokens.json')
+    for saved_token in data:
+        if (saved_token['token_id'] == token):
+            return (True,saved_token['username'])
+    return (False,'The token does not exist')
+
+def verifyHeader(username):
+    #Get the Authorization header (the first position is "token", the second is the user_token) and verify the structure
+    header = request.headers.get('Authorization')
+    authHeader = header.split()
+    if(len(authHeader) != 2 or authHeader[0] != "token"):
+        return 'Incorrect authorization header. Try again with the following format: token user_token', HTTP_400_BAD_REQUEST
+
+    #Verify that the token exist and it belongs to the user that do the request
+    token_exist = verifyToken(authHeader[1])
+    user_name = token_exist[1]
+    if(token_exist[0] == False):
+        return 'Incorrect token.', HTTP_400_BAD_REQUEST
+    elif(user_name != username):
+        return 'The token of the header belongs to other user.', HTTP_400_BAD_REQUEST
+    else:
+        return True,'The authorization header is correct.'
 
 #/VERSION
 @app.route('/version', methods=['GET'])
@@ -106,79 +126,103 @@ def login():
 #/<string:username>/<string:doc_id>
 @app.route('/<string:username>/<string:doc_id>', methods=['GET'])
 def get(username, doc_id):
-    docFound = [doc for doc in DocumentList if doc['owner'] == username and doc['doc_id'] == doc_id]
-    return jsonify(docFound)
+    validate = verifyHeader(username)
+    if(validate[0] == True): 
+        docFound = [doc for doc in DocumentList if doc['owner'] == username and doc['doc_id'] == doc_id]
+        if(len(docFound) != 1):
+            return 'The user <username> does not have any document with this name.', HTTP_400_BAD_REQUEST
+        else:
+            return jsonify(docFound)
+    else:
+        return validate
 
 @app.route('/<string:username>/<string:doc_id>', methods=['POST'])
 def post(username,doc_id):
-    userFound = [users for users in UserList if users['username'] == username]
-    docFound = [doc for doc in DocumentList if doc['doc_id'] == doc_id]
-    if(len(userFound) == 1 and len(docFound) == 0):
-        try:
-            doc = {
-                "owner":username,
-                "doc_id":doc_id,
-                "doc_content":request.json['doc_content']
-            }
-        except KeyError:
-            return jsonify({'error': "Introduce the doc_content."}), HTTP_400_BAD_REQUEST
+    validate = verifyHeader(username)
+    if(validate[0] == True):
+        userFound = [users for users in UserList if users['username'] == username]
+        docFound = [doc for doc in DocumentList if doc['doc_id'] == doc_id]
+        if(len(userFound) == 1 and len(docFound) == 0):
+            try:
+                doc = {
+                    "owner":username,
+                    "doc_id":doc_id,
+                    "doc_content":request.json['doc_content']
+                }
+            except KeyError:
+                return jsonify({'error': "Introduce the doc_content."}), HTTP_400_BAD_REQUEST
 
-        DocumentList.append(doc)
-        size = sys.getsizeof(request.json['doc_content'])
+            DocumentList.append(doc)
+            size = sys.getsizeof(request.json['doc_content'])
         
-        data = read('documents.json')
-        data.append(doc)
-        write('documents.json', data)
+            data = read('documents.json')
+            data.append(doc)
+            write('documents.json', data)
         
-        return jsonify({"size": size}), HTTP_201_CREATED 
-    elif (len(docFound) > 0):
-        return 'The doc_id exist already! Try again with other doc_id', HTTP_400_BAD_REQUEST
+            return jsonify({"size": size}), HTTP_201_CREATED 
+        elif (len(docFound) > 0):
+            return 'The doc_id exist already! Try again with other doc_id', HTTP_400_BAD_REQUEST
+        else:
+            return 'The username does not exist! Try again with other username', HTTP_400_BAD_REQUEST
     else:
-        return 'The username does not exist! Try again with other username', HTTP_400_BAD_REQUEST
+        return validate
 
 @app.route('/<string:username>/<string:doc_id>', methods=['PUT'])
 def put(username, doc_id):
-    docFound = [doc for doc in DocumentList if doc['owner'] == username and doc['doc_id'] == doc_id]
-    docFound['doc_content'] = request.json['doc_content']
+    validate = verifyHeader(username)
+    if(validate[0] == True):
+        docFound = [doc for doc in DocumentList if doc['owner'] == username and doc['doc_id'] == doc_id]
+        docFound['doc_content'] = request.json['doc_content']
     
-    size = sys.getsizeof(request.json['doc_content'])
+        size = sys.getsizeof(request.json['doc_content'])
     
-    data = read('documents.json')
-    data.append(docFound)
-    write('documents.json', data)
+        data = read('documents.json')
+        data.append(docFound)
+        write('documents.json', data)
 
-    return jsonify({"size": size}), HTTP_201_CREATED 
+        return jsonify({"size": size}), HTTP_201_CREATED 
+    else:
+        return validate
 
 @app.route('/<string:username>/<string:doc_id>', methods=['DELETE'])
 def delete(username, doc_id):
-    new_docList=[]
-    global DocumentList
-    for doc in DocumentList:
-        if(doc['owner'] != username and doc['doc_id'] != doc_id):
-            new_docList.append(doc)
-    write('documents.json', new_docList)
+    validate = verifyHeader(username)
+    if(validate[0] == True):
+        new_docList=[]
+        global DocumentList
+        for doc in DocumentList:
+            if(doc['owner'] != username and doc['doc_id'] != doc_id):
+                new_docList.append(doc)
+        write('documents.json', new_docList)
     
-    return jsonify({}), HTTP_201_CREATED 
+        return jsonify({}), HTTP_201_CREATED 
+    else:
+        return validate
 
 #/<string:username>/_all_docs
 @app.route('/<string:user_name>/all_docs' , methods=['GET'])
 def get_all_docs(user_name):
-    coincidence = False
-    counter = 0
-    new_list=[]
-    for documents in DocumentList:
-        if(documents['owner']  == user_name):
-            coincidence = True
-            counter += 1
-            document = {
-                "id"+str(counter): documents['doc_id'],
-                "content":documents['doc_content']
-            }
-            new_list.append(document)
-    if coincidence == False:
-        return 'The user does not have any document', HTTP_400_BAD_REQUEST
+    validate = verifyHeader(user_name)
+    if(validate[0] == True):
+        coincidence = False
+        counter = 0
+        new_list=[]
+        for documents in DocumentList:
+            if(documents['owner']  == user_name):
+                coincidence = True
+                counter += 1
+                doc_id = "id"+str(counter)
+                document = {
+                    doc_id: documents['doc_id'],
+                    "doc_content":documents['doc_content']
+                }
+                new_list.append(document)
+        if coincidence == False:
+            return 'The user does not have any document', HTTP_400_BAD_REQUEST
+        else:
+            return new_list
     else:
-        return new_list
+        return validate
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
